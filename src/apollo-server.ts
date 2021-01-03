213067b86c2f20afedd2225d25fb498ad5e1da44
Tunde Ganiy <tundaey@gmail.com>
2021-01-03 09:35:57 +0000

    testing stuff out

--- src/apollo-server.ts
+++ src/apollo-server.ts
@@ -2,5 +2,8 @@ import { ApolloServer } from 'apollo-server-lambda';
 import { resolvers } from './resolvers';
 import { typeDefs } from './type-defs';
 
-const apolloServer = new ApolloServer({ resolvers, typeDefs });
+const apolloServer = new ApolloServer({
+  resolvers,
+  typeDefs
+});
 export const graphqlHandler = apolloServer.createHandler();
